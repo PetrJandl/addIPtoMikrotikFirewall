1cfcef04dc998de726afd062392b00591066afd1
Petr Jandl <petr.jandl@gmail.com>
2026-03-13 09:29:30 +0100

    add config examples

--- application/config/routeros.example.php
+++ application/config/routeros.example.php
@@ -1,7 +1,16 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+// Login to mikrotik router
 $config['routeros_ip'] = '';
 $config['routeros_user'] = '';
 $config['routeros_pass'] = '';
+// Mail for send abnormality
+$config['whitelist_mail_from'] = '';
+$config['whitelist_mail_to'] = '';
+// reCAPTCHA
+$config['recap_site_key'] = '';
+$config['recap_secret'] = '';
+
+
+
 
