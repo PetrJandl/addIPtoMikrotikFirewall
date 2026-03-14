99155cb492bd440163224df680bb2e5c6b73dad5
Petr Jandl <petr.jandl@gmail.com>
2026-03-14 16:07:39 +0100

    chanche sql scructure

--- DB_routeros_api.sql
+++ DB_routeros_api.sql
@@ -3,8 +3,6 @@
 --
 
 DROP TABLE IF EXISTS `IMAPwhiteList`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `IMAPwhiteList` (
   `ip` varchar(15) COLLATE utf8_czech_ci NOT NULL,
   `network` varchar(32) COLLATE utf8_czech_ci DEFAULT NULL,
@@ -12,7 +10,39 @@ CREATE TABLE `IMAPwhiteList` (
   `rangeEnd` varchar(15) COLLATE utf8_czech_ci DEFAULT NULL,
   `comment` varchar(250) COLLATE utf8_czech_ci DEFAULT NULL,
   `active` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ip`),
-  UNIQUE KEY `ip` (`ip`)
+  `changed` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+--
+-- Triggers `IMAPwhiteList`
+--
+DROP TRIGGER IF EXISTS `insert`;
+DELIMITER $$
+CREATE TRIGGER `insert` BEFORE INSERT ON `IMAPwhiteList` FOR EACH ROW BEGIN
+
+SET NEW.changed = NOW();
+
+END
+$$
+DELIMITER ;
+DROP TRIGGER IF EXISTS `last_update_row`;
+DELIMITER $$
+CREATE TRIGGER `last_update_row` BEFORE UPDATE ON `IMAPwhiteList` FOR EACH ROW BEGIN
+
+SET NEW.changed = NOW();
+
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `IMAPwhiteList`
+--
+ALTER TABLE `IMAPwhiteList`
+  ADD PRIMARY KEY (`ip`),
+  ADD UNIQUE KEY `ip` (`ip`);
 
